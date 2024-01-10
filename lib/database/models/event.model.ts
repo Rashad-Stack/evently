484dc20239a983757e260381dd06d8830f901403
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-01-11 05:11:55 +0600

    Add MongoDB and Mongoose dependencies

---
+++ lib/database/models/event.model.ts
@@ -0,0 +1,39 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IEvent extends Document {
+  _id: string;
+  title: string;
+  description?: string;
+  location?: string;
+  createdAt: Date;
+  imageUrl: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  price: string;
+  isFree: boolean;
+  url?: string;
+  category: { _id: string; name: string };
+  organizer: { _id: string; firstName: string; lastName: string };
+}
+
+const eventSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    location: { type: String },
+    imageUrl: { type: String, required: true },
+    startDateTime: { type: Date, required: true, default: Date.now },
+    endDateTime: { type: Date, required: true, default: Date.now },
+    price: { type: String },
+    isFree: { type: Boolean },
+    url: { type: String },
+    category: { type: Schema.Types.ObjectId, ref: "Category" },
+    organizer: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Event = models.Event || model("Event", eventSchema);
+export default Event;
