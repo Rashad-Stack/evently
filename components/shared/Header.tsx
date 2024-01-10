1f22db8b0dbf1a2523b5eeba45ed42e4d2f84484
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-01-11 03:32:08 +0600

    Add @radix-ui/react-dialog and @radix-ui/react-separator dependencies, and update Header component

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,6 +2,8 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui/button";
+import MobileNav from "./MobileNav";
+import NavItems from "./NavItems";
 
 export default function Header() {
   return (
@@ -15,9 +17,17 @@ export default function Header() {
             alt="Evently logo"
           />
         </Link>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
         <div className="flex justify-end gap-3 w-32">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
+            <MobileNav />
           </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
