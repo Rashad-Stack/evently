7600b7c1133652d485ee09381a6b5c05cc4c06bd
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-01-12 03:45:11 +0600

    Add category functionality and fetch categories on component mount

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -17,8 +17,12 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  createCategory,
+  getAllCategories,
+} from "@/lib/actions/category.action";
 import { ICategory } from "@/lib/database/models/category.model";
-import { startTransition, useState } from "react";
+import { startTransition, useEffect, useState } from "react";
 import { Input } from "../ui/input";
 
 type DropdownProps = {
@@ -30,7 +34,21 @@ export default function Dropdown({ onChangeHandler, value }: DropdownProps) {
   const [categories, setCategories] = useState<ICategory[]>([]);
   const [newCategory, setNewCategory] = useState<string>("");
 
-  const handleAddCategory = () => {};
+  const handleAddCategory = () => {
+    createCategory({ categoryName: newCategory }).then((category) =>
+      setCategories((prevState) => [...prevState, category]),
+    );
+  };
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const categoryList = await getAllCategories();
+
+      categoryList && setCategories(categoryList as ICategory[]);
+    };
+
+    getCategories();
+  }, []);
 
   return (
     <Select onValueChange={onChangeHandler} defaultValue={value}>
@@ -47,7 +65,7 @@ export default function Dropdown({ onChangeHandler, value }: DropdownProps) {
 
         <AlertDialog>
           <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">
-            Open
+            Add New Category
           </AlertDialogTrigger>
           <AlertDialogContent className="bg-white">
             <AlertDialogHeader>
